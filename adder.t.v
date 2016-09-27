4871d7f2611e6bda6e41872649556f82aa73b4e6
apurvaraman <apurva.raman@students.olin.edu>
2016-09-26 21:18:15 -0400

    Adding code for 4 bit adder.

---
+++ adder.t.v
@@ -0,0 +1,60 @@
+// Adder testbench
+`timescale 1 ns / 1 ps
+`include "adder.v"
+
+// module testFullAdder() ;
+//     reg a, b, carryin;
+//     wire sum, carryout;
+//
+//     structuralFullAdder adder (sum, carryout, a, b, carryin);
+//
+//
+//     initial begin
+//     $dumpfile("adder.vcd");
+//     $dumpvars(0, testFullAdder);
+//
+//     $display("A B Carryin | Sum Carryout | Expected Output");
+//     a=0;b=0;carryin=0; #1000
+//     $display("%b %b    %b    |    %b     %b   | 0  0", a, b, carryin, sum, carryout);
+//     a=0;b=0;carryin=1; #1000
+//     $display("%b %b    %b    |    %b     %b   | 1  0", a, b, carryin, sum, carryout);
+//     a=0;b=1;carryin=0; #1000
+//     $display("%b %b    %b    |    %b     %b   | 1  0", a, b, carryin, sum, carryout);
+//     a=0;b=1;carryin=1; #1000
+//     $display("%b %b    %b    |    %b     %b   | 0  1", a, b, carryin, sum, carryout);
+//     a=1;b=0;carryin=0; #1000
+//     $display("%b %b    %b    |    %b     %b   | 1  0", a, b, carryin, sum, carryout);
+//     a=1;b=0;carryin=1; #1000
+//     $display("%b %b    %b    |    %b     %b   | 0  1", a, b, carryin, sum, carryout);
+//     a=1;b=1;carryin=0; #1000
+//     $display("%b %b    %b    |    %b     %b   | 0  1", a, b, carryin, sum, carryout);
+//     a=1;b=1;carryin=1; #1000
+//     $display("%b %b    %b    |    %b     %b   | 1  1", a, b, carryin, sum, carryout);
+//     end
+// endmodule
+
+module testFourBit();
+    reg carryin;
+    reg[3:0] a, b;
+    wire[3:0] sum;
+    wire carryout0, carryout1, carryout2, carryout3;
+
+    fourBitAdder fouradder (sum, carryout3, overflow, a, b, carryin);
+
+    initial begin
+    $dumpfile("adder.vcd");
+    $dumpvars(0, testFourBit);
+
+    $display("   A   B   Carryin |  Sum   Carryout   |  Overflow  | Expected Output | Expected Carryout");
+    a=0000;b=0000;carryin=0; #1000
+    $display(" %b %b    %b    |  %b      %b      |      %b     |       0000      |     0    ", a, b, carryin, sum, carryout3, overflow);
+    a=0001;b=0000;carryin=0; #1000
+    $display(" %b %b    %b    |  %b      %b      |      %b     |       0001      |     0    ", a, b, carryin, sum, carryout3, overflow);
+    a=0001;b=0001;carryin=0; #1000
+    $display(" %b %b    %b    |  %b      %b      |      %b     |       0010      |     0    ", a, b, carryin, sum, carryout3, overflow);
+    a=1000;b=1000;carryin=0; #1000
+    $display(" %b %b    %b    |  %b      %b      |      %b     |       0000      |     1    ", a, b, carryin, sum, carryout3, overflow);
+    a=1100;b=0110;carryin=0; #1000
+    $display(" %b %b    %b    |  %b      %b      |      %b     |       1010      |     1    ", a, b, carryin, sum, carryout3, overflow);
+    end
+endmodule
---
+++ adder.v
@@ -0,0 +1,52 @@
+// Add delays
+`define AND and #50
+`define OR or #50
+`define NOT not #50
+`define XOR xor #50
+
+module behavioralFullAdder
+(
+    output sum,
+    output carryout,
+    input a,
+    input b,
+    input carryin
+);
+    // Uses concatenation operator and built-in '+'
+    assign {carryout, sum}=a+b+carryin;
+endmodule
+
+module structuralFullAdder
+(
+    output sum,
+    output carryout,
+    input a,
+    input b,
+    input carryin
+);
+wire AxorB, AxorBandcarryin, AandB;
+`XOR xorgate1(AxorB, a, b);
+`XOR xorgate2(sum, AxorB, carryin);
+`AND andgate1(AxorBandcarryin, AxorB, carryin);
+`AND andgate2(AandB, a, b);
+`OR orgate(carryout, AxorBandcarryin, AandB);
+endmodule
+
+module fourBitAdder
+(
+    output [3:0] sum,
+    output carryout3,
+    output overflow,
+    input [3:0] a,
+    input [3:0] b,
+    input carryin
+);
+  wire carryout0, carryout1, carryout2, carryout3;
+  structuralFullAdder add1(sum[0], carryout0, a[0], b[0], carryin);
+  structuralFullAdder add2(sum[1], carryout1, a[1], b[1], carryout0);
+  structuralFullAdder add3(sum[2], carryout2, a[2], b[2], carryout1);
+  structuralFullAdder add4(sum[3], carryout3, a[3], b[3], carryout2);
+  `XOR overflowgate(overflow,carryout2,carryout3);
+
+
+endmodule
