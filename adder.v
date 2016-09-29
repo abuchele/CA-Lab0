b28026e311b9c6065aaa3f7b8e3e1ad41dc16eea
abuchele <anna.buchele@students.olin.edu>
2016-09-28 20:21:09 -0400

    final adder verilog

--- adder.v
+++ adder.v
@@ -32,17 +32,16 @@ wire AxorB, AxorBandcarryin, AandB;
 `OR orgate(carryout, AxorBandcarryin, AandB);
 endmodule
 
-module fourBitAdder
+module FullAdder4bit
 (
     output [3:0] sum,
     output carryout,
     output overflow,
     input [3:0] a,
-    input [3:0] b,
-    input carryin
+    input [3:0] b
 );
   wire carryout0, carryout1, carryout2, carryout;
-  structuralFullAdder add1(sum[0], carryout0, a[0], b[0], carryin);
+  structuralFullAdder add1(sum[0], carryout0, a[0], b[0], 0);
   structuralFullAdder add2(sum[1], carryout1, a[1], b[1], carryout0);
   structuralFullAdder add3(sum[2], carryout2, a[2], b[2], carryout1);
   structuralFullAdder add4(sum[3], carryout, a[3], b[3], carryout2);
