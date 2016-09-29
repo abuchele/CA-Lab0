854190637e2cd211e8e03777c28ff126ff81b4fa
abuchele <anna.buchele@students.olin.edu>
2016-09-28 21:37:35 -0400

    Add files via upload
    commented code

--- adder.v
+++ adder.v
@@ -4,7 +4,7 @@
 `define NOT not #50
 `define XOR xor #50
 
-module behavioralFullAdder
+/*module behavioralFullAdder
 (
     output sum,
     output carryout,
@@ -14,10 +14,11 @@ module behavioralFullAdder
 );
     // Uses concatenation operator and built-in '+'
     assign {carryout, sum}=a+b+carryin;
-endmodule
+endmodule*/
 
 module structuralFullAdder
 (
+  // This is the unit module which the FullAdder4bit runs 4 times
     output sum,
     output carryout,
     input a,
@@ -25,10 +26,13 @@ module structuralFullAdder
     input carryin
 );
 wire AxorB, AxorBandcarryin, AandB;
+// XOR gate is true only if one of the two inputs is true, not both
 `XOR xorgate1(AxorB, a, b);
 `XOR xorgate2(sum, AxorB, carryin);
+// AND gate is true only if both inputs are true
 `AND andgate1(AxorBandcarryin, AxorB, carryin);
 `AND andgate2(AandB, a, b);
+/// OR gate is true if either or both of the inputs are true
 `OR orgate(carryout, AxorBandcarryin, AandB);
 endmodule
 
@@ -41,10 +45,13 @@ module FullAdder4bit
     input [3:0] b
 );
   wire carryout0, carryout1, carryout2, carryout;
+  // This runs the structuralFullAdder four times, once for each 'bit'
+  // For the inital run, we don't need to use a carryin. The carryin is relevant for the other four runs. 
   structuralFullAdder add1(sum[0], carryout0, a[0], b[0], 0);
   structuralFullAdder add2(sum[1], carryout1, a[1], b[1], carryout0);
   structuralFullAdder add3(sum[2], carryout2, a[2], b[2], carryout1);
   structuralFullAdder add4(sum[3], carryout, a[3], b[3], carryout2);
+  // Overflow occurs if the carryout and carryin are not the same. For this case an XOR gate is optimal.
   `XOR overflowgate(overflow,carryout2,carryout);
 
 
